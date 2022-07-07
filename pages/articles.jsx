6b00c617cce886ef85a3e1cd8f7371ad0a866693
Keelan <keelanmatthews123@gmail.com>
2022-07-07 20:17:06 +0200

    Styling articles

--- pages/articles.jsx
+++ pages/articles.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import DoctorOfYear from '../components/articles-page/DoctorOfYear'
 
 export default function articles() {
-  return (
-    <Layout pageTitle="RuDASA | Articles">
-
+	return (
+		<Layout pageTitle="RuDASA | Articles">
+			<DoctorOfYear/>
 		</Layout>
-  )
+	)
 }
