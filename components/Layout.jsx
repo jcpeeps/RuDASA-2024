4148f3f6a04ea07d6f62f2fcae846d44f9054147
Keelan <keelanmatthews123@gmail.com>
2022-07-07 11:14:30 +0200

    Set up bootstrap and Layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Head from 'next/head';
+const Layout = (props) => {
+    return (
+        <>
+            <Head>
+                <title>{props.pageTitle}</title>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+            </Head>
+            <main>
+                {props.children}
+            </main>
+        </>
+    )
+}
+export default Layout;
