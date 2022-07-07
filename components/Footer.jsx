b8646baf20ff63b424a51a5f8cbdb02d8309b168
taylawantsasnack <tayla.orsmond@gmail.com>
2022-07-07 18:21:32 +0200

    Changed Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSprout } from '@fortawesome/free-solid-svg-icons'
+import { faShareNodes } from '@fortawesome/free-solid-svg-icons'
+import { faHandHoldingMedical } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
 	return (
@@ -15,19 +18,32 @@ export default function Footer() {
 						</ul>
 					</div>
 					<div className="col">
-
+						<h5 className="text-white">Find a Hospital</h5>
+						<ul className="list-unstyled">
+							<li><a href="#" className="text-white text-decoration-none">Hospitals in South Africa</a></li>
+							<li><a href="#" className="text-white text-decoration-none">Government hospitals</a></li>
+							<li><a href="#" className="text-white text-decoration-none">Rural hospitals</a></li>
+						</ul>
 					</div>
 					<div className="col">
-
+						<h5 className="text-white">HST Email Discussion Groups</h5>
+						<ul className="list-unstyled">
+							<li><a href="#" className="text-white text-decoration-none">Drug Policy & Practice</a></li>
+							<li><a href="#" className="text-white text-decoration-none">Health & Human Rights</a></li>
+							<li><a href="#" className="text-white text-decoration-none">60percent</a></li>
+						</ul>
 					</div>
-					<div className="col">
-
+					<div className="col d-flex flex-column">
+						<div className="bg-white rounded p-2"><FontAwesomeIcon icon={faShareNodes}/></div>
+						<div className="bg-white rounded p-2"><FontAwesomeIcon icon={faSprout}/>Rural Seeds Forum</div>
+						<div className="bg-white rounded"></div>
 					</div>
 				</div>
 				<div className="w-100 d-flex justify-content-center">
 					<FontAwesomeIcon icon={faFacebook}/>
 					<FontAwesomeIcon icon={faTwitter}/>
 					<FontAwesomeIcon icon={faInstagram}/>
+					<FontAwesomeIcon icon={faHandHoldingMedical}/>
 				</div>
 			</div>
 		</footer>
