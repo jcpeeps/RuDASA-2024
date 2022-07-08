4ae670b8299bc3042cd608e0802324a7ffd5097d
Keelan <keelanmatthews123@gmail.com>
2022-07-08 13:15:48 +0200

    Finished sign up funcitonality

---
+++ components/signup-login/ProgressBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function ProgressBar(props) {
+    return (
+        <div className="w-75 ms-5">
+            <div className="progress-bar">
+                <div
+                    className="progress-loader"
+                    style={{
+                        width: props.step === 0 ? "0%" :
+                            props.step === 1 ? "50%" : "100%"
+                    }}
+                />
+            </div>
+            <div className="d-flex justify-content-between w-100 progress-offset">
+                <div className="d-flex flex-column align-items-center">
+                    <div className="rounded-circle bg-primary progress-circle text-white fw-bold p-1">1</div>
+                    <small className="text-primary">General</small>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                    <div className="rounded-circle progress-circle text-white fw-bold p-1"
+                        style={{ background: props.step === 0 ? "#BEBEBE" : "var(--primary)" }}
+                    >
+                        2
+                    </div>
+                    <small style={{ color: props.step === 0 ? "#BEBEBE" : "var(--primary)" }}>Address</small>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                    <div className="rounded-circle progress-circle text-white fw-bold p-1"
+                        style={{ background: props.step === 2 ? "var(--primary)" : "#BEBEBE" }}
+                    >
+                        3
+                    </div>
+                    <small style={{ color: props.step === 2 ? "var(--primary)" : "#BEBEBE" }}>Health Club</small>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/signUp.jsx
+++ pages/signUp.jsx
@@ -5,6 +5,7 @@ import Illustration from '../media/svg/login.svg'
 import General from '../components/signup-login/General'
 import Address from '../components/signup-login/Address'
 import Club from '../components/signup-login/Club'
+import ProgressBar from '../components/signup-login/ProgressBar'
 
 export default function signUp() {
 
@@ -12,9 +13,9 @@ export default function signUp() {
 
     const stepDisplay = () => {
         switch (step) {
-            case 0: return <General/>
-            case 1: return <Address/>
-            case 2: return <Club/>
+            case 0: return <General />
+            case 1: return <Address />
+            case 2: return <Club />
         }
     }
 
@@ -22,29 +23,32 @@ export default function signUp() {
         <Layout pageTitle="RuDASA | Sign up">
             <section className="container">
                 <div className="py-5 mb-5"></div>
-                <div className="d-flex justify-content-center align-items-center mb-5 pb-5">
+                <div className="d-flex justify-content-center align-items-start mb-5 pb-5">
                     <Image src={Illustration} className="col-sm-12 col-md-12 col-lg-5 col-xl-5" width={600} height={600} />
-                    <h1 className="fw-bold w-100 mb-5 text-center text-primary">Sign Up</h1>
-                    <div>
-                        {stepDisplay()}
-                    </div>
-                    <div>
-                        <button className="btn btn-outline"
-                            disabled={step == 0}
-                            onClick={() => {
-                                setStep((currStep) => currStep - 1);
-                            }}
-                        >
-                            Back
-                        </button>
-                        <button className="btn btn-secondary"
-                            disabled={step == 2}
-                            onClick={() => {
-                                setStep((currStep) => currStep + 1);
-                            }}
-                        >
-                            Next
-                        </button>
+                    <div className="col-sm-12 col-md-12 col-lg-5 col-xl-5 offset-md-1 offset-lg-1 d-flex flex-column align-items-center">
+                        <h1 className="fw-bold w-100 mb-5 text-center text-primary">Sign Up</h1>
+                        <ProgressBar step={step} />
+                        <div>
+                            {stepDisplay()}
+                        </div>
+                        <div>
+                            <button className="btn btn-outline"
+                                disabled={step == 0}
+                                onClick={() => {
+                                    setStep((currStep) => currStep - 1);
+                                }}
+                            >
+                                Back
+                            </button>
+                            <button className="btn btn-secondary"
+                                disabled={step == 2}
+                                onClick={() => {
+                                    setStep((currStep) => currStep + 1);
+                                }}
+                            >
+                                Next
+                            </button>
+                        </div>
                     </div>
                 </div>
             </section>
