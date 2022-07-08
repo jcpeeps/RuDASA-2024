a2160893f2a2a17ce4650e5e2d4f695cf5f8d1c0
Keelan <keelanmatthews123@gmail.com>
2022-07-08 11:01:11 +0200

    Moved resource component

--- components/resources/ResourceGroups.jsx
+++ components/resources/ResourceGroups.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ResourceCard from '../resources/ResourceCard'
+import ResourceCard from '../ResourceCard'
 import Icon from '../../media/icons/icon-01.svg'
 
 export default function ResourceGroups() {
