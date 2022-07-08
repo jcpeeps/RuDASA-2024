83f3f545d8e175ea2d59ce3b20f8efbd864c0256
Keelan <keelanmatthews123@gmail.com>
2022-07-08 10:59:57 +0200

    Fixed resource cards

--- components/resources/ResourceCard.jsx
+++ components/resources/ResourceCard.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import Icon from '../../media/icons/icon.png'
 
 export default function ResourceCard(props) {
   return (
     <div className="resc-card-width bg-white shadow rounded p-4 m-3 d-flex flex-column align-items-center justify-content-center">
         <a href="#" className="text-black text-decoration-none">
             <p className="fw-bold text-center">{props.title}</p>
-            <div className="d-flex justify-content-center"><Image src={Icon}/></div> 
+            <div className="d-flex justify-content-center"><Image src={props.icon}/></div> 
         </a>
     </div>
   )
--- components/resources/ResourceGroups.jsx
+++ components/resources/ResourceGroups.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import ResourceCard from '../resources/ResourceCard'
+import Icon from '../../media/icons/icon-01.svg'
 
 export default function ResourceGroups() {
   return (
     <div className="container my-5">
         <h3 className="fw-bold">Students &amp; Young Doctors</h3>
         <div className="col-12 col-lg-8 d-flex flex-wrap p-4">
-            <ResourceCard icon="" title="Student Electives"/>
-            <ResourceCard icon="" title="Scholarships"/>
-            <ResourceCard icon="" title="Student Rural Health Clubs"/>
-            <ResourceCard icon="" title="Helplines"/>
+            <ResourceCard icon={Icon} title="Student Electives"/>
+            <ResourceCard icon={Icon} title="Scholarships"/>
+            <ResourceCard icon={Icon} title="Student Rural Health Clubs"/>
+            <ResourceCard icon={Icon} title="Helplines"/>
         </div>
         <h3 className="fw-bold">Rural Resources</h3>
         <div className="col-12 col-lg-8  d-flex flex-wrap p-4">
-            <ResourceCard icon="" title="Rural Mental Health"/>
-            <ResourceCard icon="" title="Rural Policy &amp; HR"/>
-            <ResourceCard icon="" title="Rural Contacts"/>
-            <ResourceCard icon="" title="Community Engagement"/>
+            <ResourceCard icon={Icon} title="Rural Mental Health"/>
+            <ResourceCard icon={Icon} title="Rural Policy &amp; HR"/>
+            <ResourceCard icon={Icon} title="Rural Contacts"/>
+            <ResourceCard icon={Icon} title="Community Engagement"/>
         </div>
         <h3 className="fw-bold">Other Resources</h3>
         <div className="col-12 col-lg-8  d-flex flex-wrap p-4">
-            <ResourceCard icon="" title="Labour Relations"/>
-            <ResourceCard icon="" title="Acts &amp; Policies"/>
-            <ResourceCard icon="" title="Systems &amp; Advocacy"/>
-            <ResourceCard icon="" title="Telemedecine"/>
-            <ResourceCard icon="" title="COVID-19"/>
-            <ResourceCard icon="" title="PHC &amp; Guidelines"/>
-            <ResourceCard icon="" title="Research &amp; Quick Links"/>
-            <ResourceCard icon="" title="Courses"/>
+            <ResourceCard icon={Icon} title="Labour Relations"/>
+            <ResourceCard icon={Icon} title="Acts &amp; Policies"/>
+            <ResourceCard icon={Icon} title="Systems &amp; Advocacy"/>
+            <ResourceCard icon={Icon} title="Telemedecine"/>
+            <ResourceCard icon={Icon} title="COVID-19"/>
+            <ResourceCard icon={Icon} title="PHC &amp; Guidelines"/>
+            <ResourceCard icon={Icon} title="Research &amp; Quick Links"/>
+            <ResourceCard icon={Icon} title="Courses"/>
         </div>
     </div>
   )
