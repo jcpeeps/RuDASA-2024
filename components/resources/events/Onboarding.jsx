12a0fefe81f0089f58b6172bb495b97092d799b4
Keelan <keelanmatthews123@gmail.com>
2022-07-09 16:52:41 +0200

    Fixed onboarding styling

--- components/resources/events/Onboarding.jsx
+++ components/resources/events/Onboarding.jsx
@@ -5,10 +5,10 @@ export default function SnT () {
         <div className="d-flex flex-column my-5">
             <h4 className="w-100 fw-bold display-6 text-primary">Rural Health On-Boarding Programme</h4>
             <h5 className="w-100 fw-bold">31st January to 13th March 2022</h5>
-            <div className="d-flex flex-wrap col-12 col-lg-10 my-5">
+            <div className="d-flex flex-wrap col-12 col-lg-12 my-5">
                 <h5 className="fw-bold">Learn how to thrive, not just survive, in rural health!</h5>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et minima autem officia soluta! Recusandae placeat eos nihil, corrupti nobis quidem repudiandae modi numquam aliquid debitis dolorem earum ratione ad cumque quasi ipsum a ullam optio repellendus tenetur cupiditate. Accusantium culpa odio impedit maiores voluptates, reiciendis veniam inventore accusamus, excepturi saepe maxime recusandae dolor architecto? Sequi dicta sapiente nisi ad laboriosam!</p>
-                <div className="d-flex flex-wrap flex-lg-nowrap justify-content-between my-5">
+                <div className="d-flex flex-wrap flex-lg-nowrap justify-content-between align-items-start my-5">
                     <div className="d-flex flex-column col-12 col-lg-6">
                         <h5 className="fw-bold">Course Structure</h5>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolorum error ad dolorem fuga, assumenda veniam dolores provident ex adipisci soluta omnis fugiat a laborum similique ratione molestias dicta esse aut dolor voluptatem doloribus. Accusamus, dolor dolores ducimus doloremque, a consequuntur eaque placeat impedit qui ex porro commodi repudiandae nostrum delectus sunt quos eum asperiores maiores? Deserunt nemo quod porro voluptatem doloribus placeat ipsum, soluta molestiae laborum culpa quam ipsa! Deserunt, numquam doloremque eveniet et eos, veniam, laboriosam sunt laborum aliquam vero cum ratione tempora voluptatem eius autem debitis voluptate distinctio dolorum consequatur ad rem explicabo! Exercitationem quidem distinctio praesentium!</p>  
@@ -17,7 +17,7 @@ export default function SnT () {
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione nihil eius, laudantium necessitatibus, harum velit quidem officia ut suscipit reprehenderit excepturi veniam architecto. Perspiciatis corporis cupiditate optio culpa fuga dignissimos necessitatibus velit dolor. Sequi amet officiis cum tenetur atque deserunt.</p>
                     </div>
                 </div>
-                <div className="d-flex flex-wrap flex-lg-nowrap justify-content-between my-5">
+                <div className="d-flex flex-wrap flex-lg-nowrap align-items-start justify-content-between my-5">
                     <div className="d-flex flex-column col-12 col-lg-6">
                         <h5 className="fw-bold">Curriculum Overview</h5>
                         <p>There are 3 "types" of orientation topics</p>
@@ -39,7 +39,7 @@ export default function SnT () {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolorum error ad dolorem fuga, assumenda veniam dolores provident ex adipisci soluta omnis fugiat a laborum similique ratione molestias dicta esse aut dolor voluptatem doloribus. Accusamus, dolor dolores ducimus doloremque, a consequuntur eaque placeat impedit qui ex porro commodi repudiandae nostrum delectus sunt quos eum asperiores maiores? Deserunt nemo quod porro voluptatem doloribus placeat ipsum, soluta molestiae laborum culpa quam ipsa! Deserunt, numquam doloremque eveniet et eos, veniam, laboriosam sunt laborum aliquam vero cum ratione tempora voluptatem eius autem debitis voluptate distinctio dolorum consequatur ad rem explicabo! Exercitationem quidem distinctio praesentium!</p>  
                     </div>
                 </div>
-                <div className="d-flex flex-wrap flex-lg-nowrap justify-content-between my-5">
+                <div className="d-flex flex-wrap flex-lg-nowrap justify-content-between align-items-start my-5">
                     <div className="d-flex flex-column col-12 col-lg-6">
                         <h5 className="fw-bold text-primary mb-3">How to get Started:</h5>
                         <p className="fw-bold">FREE for members of RuDASA, RuReSA, RuNurSA and PACASA</p>
