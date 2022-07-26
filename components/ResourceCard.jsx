23472e7fa5be3ec77eda880e2d17014ee57067ce
Keelan Matthews <keelanmatthews123@gmail.com>
2022-07-26 10:09:29 +0200

    Fixed resource responsiveness

--- components/ResourceCard.jsx
+++ components/ResourceCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function ResourceCard(props) {
   return (
-    <div className="resc-card-width bg-white shadow rounded p-4 m-3 d-flex flex-column align-items-center justify-content-center">
+    <div className="resc-card-width bg-white shadow rounded p-4 m-1 m-md-3 d-flex flex-column align-items-center justify-content-center">
         <a href={props.link} className="text-black text-decoration-none">
             <p className="fw-bold text-center">{props.title}</p>
             <div className="d-flex justify-content-center">
--- components/resources/ResourceGroups.jsx
+++ components/resources/ResourceGroups.jsx
@@ -5,20 +5,20 @@ export default function ResourceGroups({ student, rural, other }) {
 
 	return (
 		<div className="container my-5 px-3 px-lg-0">
-			<h3 className="fw-bold">Students &amp; Young Doctors</h3>
-			<div className="d-flex flex-wrap p-3">
+			<h3 className="fw-bold mb-4 mb-md-0">Students &amp; Young Doctors</h3>
+			<div className="d-flex flex-wrap p-1 p-md-3">
 				{student.map((card, index) => (
 					<ResourceCard icon={`/icons/${card.slug}.svg`} title={card.frontmatter.title} link={`/resources/students/${card.slug}`} key={index} />
 				))}
 			</div>
 			<h3 className="fw-bold">Rural Resources</h3>
-			<div className="d-flex flex-wrap p-3">
+			<div className="d-flex flex-wrap p-1 p-md-3">
 				{rural.map((card, index) => (
 					<ResourceCard icon={`/icons/${card.slug}.svg`} title={card.frontmatter.title} link={`/resources/rural/${card.slug}`} key={index} />
 				))}
 			</div>
 			<h3 className="fw-bold">Other Resources</h3>
-			<div className="d-flex flex-wrap p-3">
+			<div className="d-flex flex-wrap p-1 p-md-3">
 				{other.map((card, index) => (
 					<ResourceCard icon={`/icons/${card.slug}.svg`} title={card.frontmatter.title} link={`/resources/other/${card.slug}`} key={index} />
 				))}
