bc38a394f6858cee07f4c618eb81282783c014cf
Keelan <keelanmatthews123@gmail.com>
2022-08-21 21:18:25 +0200

    started gallery

---
+++ pages/gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Masonry from 'react-masonry-component'
+
+export default function gallery() {
+  return (
+    <Layout pageTitle="RuDASA | Gallery">
+        {/* Start here */}
+	</Layout>
+  )
+}
