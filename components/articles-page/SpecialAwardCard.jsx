e6c73b3ecaf94e8fec1a850f271e56cf586b86f2
Wian Koekemoer <105972926+slaaiblaar@users.noreply.github.com>
2024-08-05 17:23:36 +0200

    Add Dr Fredlund Lifetime Achievement Award

--- components/articles-page/SpecialAwardCard.jsx
+++ components/articles-page/SpecialAwardCard.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock, faCalendarAlt  } from '@fortawesome/free-regular-svg-icons'
+import { faCalendarAlt, faClock } from '@fortawesome/free-regular-svg-icons';
 import { faHouseMedical, faLocationDot } from '@fortawesome/free-solid-svg-icons';
-import { marked } from 'marked'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { marked } from 'marked';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 export default function SpecialAwardCard(props) {
     const [readMore, setReadMore] = useState(false)
@@ -15,14 +15,13 @@ export default function SpecialAwardCard(props) {
                 <div className="col-sm-12 col-md-12 col-lg-5 d-flex flex-wrap flex-md-nowrap align-items-start">
                     <div className="ms-0 ms-md-2 my-3">
                     { props.image && <Image src={props.image} width={270} height={270} className="image-crop" alt="" />}
-                        <div className="d-flex align-items-center">
-                            <FontAwesomeIcon icon={faHouseMedical} className="text-primary me-2" />
-                            <p className="m-0">{props.hospital}</p>
-                        </div>
-                        <div className="d-flex align-items-center">
+                        { props.hospital && <div className="d-flex align-items-center">
+                            <FontAwesomeIcon icon={faHouseMedical} className="text-primary me-2" /><p className="m-0">{props.hospital}</p> 
+                        </div> }
+                        { props.location && <div className="d-flex align-items-center">
                             <FontAwesomeIcon icon={faLocationDot} className="text-primary me-2" />
                             <p className="m-0">{props.location}</p>
-                        </div>
+                        </div> }
                         <div className="d-flex align-items-center">
                             <FontAwesomeIcon icon={faCalendarAlt} className="text-primary me-2" />
                             <p className="m-0">{props.year}</p>
